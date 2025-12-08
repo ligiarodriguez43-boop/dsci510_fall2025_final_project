41ecb6cfd0591ae3e1110f23b71344242c08cef7
ligiarodriguez43-boop <ligia.rodriguez43@gmail.com>
2025-12-08 01:06:20 -0800

    Update tests.py

--- SRC/tests.py
+++ SRC/tests.py
@@ -84,3 +84,53 @@ class TestMLPipelines(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
+
+import requests
+
+# API text file
+KEY_FILE = 'API.txt'
+with open(KEY_FILE, "r") as f:
+    key = f.readline().strip()
+
+query = """
+query GetMeasurePopulationData {
+  measure_A(metricId: 171) {
+    measureId
+    name
+    description
+    unitType
+    format
+    source {
+      name
+    }
+    subpopulations {
+      name
+      population {
+        name
+        populationCategory {
+          name
+        }
+      }
+      data(where: { state: { in: ["ALL"] } }) {
+        endDate
+        dateLabel
+        value
+      }
+    }
+  }
+}
+"""
+
+url = 'https://api.americashealthrankings.org/graphql'
+headers = {
+    'Content-Type': 'application/json',
+    'X-Api-Key': key
+}
+
+response = requests.post(url, json={'query': query}, headers=headers)
+print(response.json())
+if response.status_code != 200:
+    raise Exception(f"GraphQL API request failed with status {response.status_code}:\n{response.text}")
+
+
